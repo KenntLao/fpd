63f17b68fcf33532ee20b51ca2f8c969877d207c
Kennt Lao <kenntlao@gmail.com>
2020-06-01 08:45:55 +0800

    Update navbar.blade.php

--- hris/resources/views/vendor/adminlte/partials/navbar/navbar.blade.php
+++ hris/resources/views/vendor/adminlte/partials/navbar/navbar.blade.php
@@ -30,10 +30,22 @@
                 @include('adminlte::partials.navbar.logout-link')
             @endif
         @endif
-
+        @if(isset($_SESSION['sys_id']))
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+        @endif
         {{-- Right sidebar toggler link --}}
         @if(config('adminlte.right_sidebar'))
-            @include('adminlte::partials.navbar.right-sidebar-link')
+        @include('adminlte::partials.navbar.right-sidebar-link')
         @endif
     </ul>
 
