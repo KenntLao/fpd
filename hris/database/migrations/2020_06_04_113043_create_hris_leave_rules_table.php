eff2707bcee9db6849efae3350b9646342ab39b1
Anthony Bulacan <anthonybulacan@yahoo.com>
2020-06-04 13:51:24 +0800

    Update

--- hris/database/migrations/2020_06_04_113043_create_hris_leave_rules_table.php
+++ hris/database/migrations/2020_06_04_113043_create_hris_leave_rules_table.php
@@ -16,6 +16,23 @@ class CreateHrisLeaveRulesTable extends Migration
         Schema::create('hris_leave_rules', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('leave_type');
+            $table->string('leave_group');
+            $table->string('job_title');
+            $table->string('employment_status');
+            $table->string('employee');
+            $table->string('exp_days');
+            $table->string('department');
+            $table->string('leave_period');
+            $table->string('supervisor_leave_assign');
+            $table->string('employee_can_apply');
+            $table->string('apply_beyond_current');
+            $table->string('leave_accrue');
+            $table->string('carried_forward');
+            $table->string('carried_forward_percentage');
+            $table->string('max_carried_forward_amount');
+            $table->string('carried_forward_leave_availability');
+            $table->string('proportionate_on_joined_date');
         });
     }
 
