da06cf2b44a365cc43fc4622cb30bb82b17c7212
Anthony Bulacan <anthonybulacan@yahoo.com>
2020-06-19 16:40:37 +0800

    Update - 19/06/2020
    Employee Seeder
    -need to assign supervisor to each employee manually

---
+++ hris/database/factories/EmployeeFactory.php
@@ -0,0 +1,66 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\hris_employee;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Faker\Generator as Faker;
+
+$factory->define(App\hris_employee::class, function (Faker $faker) {
+	$gender = $faker->randomElement(['male', 'female']);
+	$firstname = $faker->firstName($gender);
+	$lastname = $faker->lastName;
+	$fn = substr($firstname, 0, 1);
+	$ln = substr($lastname, 0, 1);
+	$employee_number = $faker->unique()->numerify('####');
+	$username = $fn.''.$ln.''.$employee_number;
+	$role_id = ''.$faker->randomElement([',4,', ',5,']);
+    return [
+        //
+        'employee_number' => $employee_number,
+        'employee_photo' => $faker->imageUrl($width = 640, $height = 480),
+        'username' => $username,
+        'password' => Hash::make(1234),
+        'firstname' => $firstname,
+        'middlename' => NULL,
+        'lastname' => $lastname,
+        'nationality' => 'Filipino',
+        'birthday' => $faker->date('Y-m-d'),
+        'gender' => $gender,
+        'place_birth' => $faker->address,
+        'dependant' => NULL,
+        'marital_status' => $faker->randomElement(['single', 'married', 'widower', 'divorced']),
+        'work_address' => $faker->address,
+        'home_address' => $faker->address,
+        'home_distance' => NULL,
+        'emergency_contact' => $faker->name,
+        'emergency_no' => $faker->phoneNumber,
+        'cert_level' => $faker->randomElement(['bachelor', 'master', 'doctoral']),
+        'field_study' => $faker->randomElement(['Lorem', 'Ipsum', 'Dolor']),
+        'school' => $faker->randomElement(['Lorem', 'Ipsum', 'Dolor']),
+        'visa_no' => NULL,
+        'work_permit' => NULL,
+        'visa_expire' => NULL,
+        'passport_no' => NULL,
+        'pagibig' => NULL,
+        'sss' => NULL,
+        'phic' => NULL,
+        'bank_acc' => $faker->unique()->numerify('############'),
+        'employment_status' => $faker->randomElement(['regular', 'co-terminus', 'probationary', 'fixed-term']),
+        'work_no' => $faker->phoneNumber,
+        'work_phone' => $faker->phoneNumber,
+        'work_email' => $faker->companyEmail,
+        'private_email' => $faker->freeEmail,
+        'joined_date' => $faker->date('Y-m-d'),
+        'termination_date' => NULL,
+        'job_title_id' => $faker->numberBetween($min = 1, $max = 75),
+        'department_id' => $faker->numberBetween($min = 1, $max = 26),
+        'supervisor' => NULL,
+        'role_id' => $role_id,
+        'last_login' => '0',
+        'status' => 'active'
+
+
+    ];
+});
