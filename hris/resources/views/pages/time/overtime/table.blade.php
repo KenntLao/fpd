9b62cba9298bc5c19ffb8cf784e2ed7d3134b1aa
Anthony Bulacan <anthonybulacan@yahoo.com>
2020-10-22 13:35:27 +0800

    UPDATE - 22/10/2020

--- hris/resources/views/pages/time/overtime/table.blade.php
+++ hris/resources/views/pages/time/overtime/table.blade.php
@@ -1,18 +1,83 @@
 <table>
     <thead>
-    <tr>
-        <th>COMPANY ID</th>
-        <th>EMPLOYEE NO</th>
-        <th>LASTNAME</th>
-        <th>FIRSTNAME</th>
-        <th>DEPARTMENT</th>
-        @foreach($types as $type)
-        <th>{{$type->name}}</th>
-        @endforeach
-    </tr>
+        <tr>
+            <th>COMPANY ID</th>
+            <th>EMPLOYEE NO</th>
+            <th>LASTNAME</th>
+            <th>FIRSTNAME</th>
+            <th>DEPARTMENT</th>
+            <th>REG</th>
+            <th>REG_>8</th>
+            <th>REG_ND1</th>
+            <th>REG_ND2</th>
+            <th>RST</th>
+            <th>RST_>8</th>
+            <th>RST_ND1</th>
+            <th>RST_ND2</th>
+            <th>LGL</th>
+            <th>LGL_>8</th>
+            <th>LGL_ND1</th>
+            <th>LGL_ND2</th>
+            <th>LGLRST</th>
+            <th>LGLRST_>8</th>
+            <th>LGLRST_ND1</th>
+            <th>LGLRST_ND2</th>
+            <th>SPL</th>
+            <th>SPL_>8</th>
+            <th>SPL_ND1</th>
+            <th>SPL_ND2</th>
+            <th>SPLRST</th>
+            <th>SPLRST_>8</th>
+            <th>SPLRST_ND1</th>
+            <th>SPLRST_ND2 }}</th>
+            <th>SPRS_CLIEN</th>
+            <th>SPRS_CLIEN_>8</th>
+            <th>SPRS_CLIEN_ND1</th>
+            <th>SPRS_CLIEN_ND2</th>
+            <th>LGRS_CLIEN</th>
+            <th>LGRS_CLIEN_8</th>
+            <th>LGRS_CLIEN_ND1</th>
+            <th>LGRS_CLIEN_ND2</th>
+            <th>SPL_CLIENT</th>
+            <th>SPL_CLIENT_>8</th>
+            <th>SPL_CLIENT_ND1</th>
+            <th>SPL_CLIENT_ND2</th>
+            <th>RST_CLIENT</th>
+            <th>RST_CLIENT_>8</th>
+            <th>RST_CLIENT_ND1</th>
+            <th>RST_CLIENT_ND2</th>
+            <th>REG_CLIENT</th>
+            <th>REG_CLIENT_>8</th>
+            <th>REG_CLIENT_ND1</th>
+            <th>REG_CLIENT_ND2</th>
+            <th>REGND_CLIE</th>
+            <th>REGND_CLIE_>8</th>
+            <th>REGND_CLIE_ND1</th>
+            <th>REGND_CLIE_ND2</th>
+            <th>LG_CLIENT</th>
+            <th>LG_CLIENT_>8</th>
+            <th>LG_CLIENT_ND1</th>
+            <th>LG_CLIENT_ND2</th>
+            <th>LGLSPL</th>
+            <th>LGLSPL_>8</th>
+            <th>LGLSPL_ND1</th>
+            <th>LGLSPL_ND2</th>
+            <th>LGLSPLRST</th>
+            <th>LGLSPLRST_>8</th>
+            <th>LGLSPLRST_ND1</th>
+            <th>LGLSPLRST_ND2</th>
+            <th>LGLSPL_CLI</th>
+            <th>LGLSPL_CLI_>8</th>
+            <th>LGLSPL_CLI_ND1</th>
+            <th>LGLSPL_CLI_ND2</th>
+            <th>LGLSPL_ND1_2</th>
+            <th>LGLSPL_ND1_2_>8</th>
+            <th>LGLSPL_ND1_2_ND1</th>
+            <th>LGLSPL_ND1_2_ND2</th>
+        </tr>
     </thead>
     <tbody>
-    @foreach($overtimes as $overtime)
+        @foreach($overtimes as $overtime)
         <tr>
             <td>FPD ASIA</td>
             <td>{{ $overtime->employee->employee_number }}</td>
@@ -88,6 +153,6 @@
             <td>{{ $overtime->LGLSPL_ND1_2_ND1_SUM }}</td>
             <td>{{ $overtime->LGLSPL_ND1_2_ND2_SUM }}</td>
         </tr>
-    @endforeach
+        @endforeach
     </tbody>
 </table>
